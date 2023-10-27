4ffd2528faf0e87d89ac5851630d48edd0ccb925
João Pedro Martinho <up202204883@up.pt>
2023-10-27 11:19:45 +0100

    Corner Cases

---
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -0,0 +1,51 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListAggregatorTest {
+    private List<Integer> list;
+    private ListAggregator aggregator;
+    @BeforeEach
+    public void helper(){
+             list = Arrays.asList(1,2,4,2,5);
+             aggregator = new ListAggregator();
+    }
+    @Test
+    public void max_bug_7623(){
+        List<Integer> l= Arrays.asList(-1,-1,-5);
+        int max = aggregator.max(l);
+        Assertions.assertEquals(-1, max);
+    }
+    @Test
+    public void sum() {
+        int sum = aggregator.sum(list);
+
+        Assertions.assertEquals(14, sum);
+    }
+
+    @Test
+    public void max() {
+        int max = aggregator.max(list);
+
+        Assertions.assertEquals(5, max);
+    }
+
+    @Test
+    public void min() {
+        int min = aggregator.min(list);
+
+        Assertions.assertEquals(1, min);
+    }
+
+    @Test
+    public void distinct() {
+        int distinct = aggregator.distinct(list);
+
+        Assertions.assertEquals(4, distinct);
+    }
+}
---
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -0,0 +1,27 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListDeduplicatorTest {
+    private List<Integer> list;
+    private List<Integer> expected;
+    private ListDeduplicator deduplicator;
+    @BeforeEach
+    public void helper(){
+        list = Arrays.asList(1,2,4,2,5);
+        expected = Arrays.asList(1,2,4,5);
+        deduplicator = new ListDeduplicator();
+    }
+
+    @Test
+    public void deduplicate() {
+        List<Integer> distinct = deduplicator.deduplicate(list);
+
+        Assertions.assertEquals(expected, distinct);
+    }
+}
